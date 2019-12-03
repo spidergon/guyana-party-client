52ca03d02b9d1db36d662ba8ee12aef4f28ca2f0
Christopher Servius <spidergon@gmail.com>
2019-12-03 13:02:33 -0300

    implementing

--- src/components/Dashboard/EditEvent.js
+++ src/components/Dashboard/EditEvent.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { navigate } from 'gatsby'
 import styled from 'styled-components'
+import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
 import EditIcon from '@material-ui/icons/Edit'
 import TextField from '@material-ui/core/TextField'
@@ -9,23 +9,28 @@ import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
-import DatePicker, { registerLocale } from 'react-datepicker'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
 import fr from 'date-fns/locale/fr'
-import 'react-datepicker/dist/react-datepicker.css'
 import Page from './Page'
 import Description from './Mde'
 import Photos from './Photos'
 import { useGroups } from '../../lib/services/groupService'
 
-registerLocale('fr', fr)
-
 const Wrapper = styled.div`
+  font-size: 1rem;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
   #name {
     max-width: 290px;
     margin: auto;
   }
   #group,
-  #dates {
+  #dates,
+  #occurrence {
     margin-top: 2.5rem;
   }
   #group {
@@ -37,16 +42,16 @@ const Wrapper = styled.div`
     }
   }
   #dates {
-    grid-template-columns: auto auto auto auto;
+    grid-template-columns: auto auto;
+    grid-gap: 2rem;
     justify-content: start;
     align-items: center;
-    input {
-      margin-left: 0.5rem;
-      padding: 5px 10px;
-      &:first-of-type {
-        margin-right: 1rem;
-      }
-    }
+  }
+  .date-error {
+    margin-top: 0.5rem;
+  }
+  #occurrence .daylabel .MuiFormControlLabel-label {
+    font-size: 0.9rem;
   }
   .mde,
   .photos,
@@ -54,48 +59,37 @@ const Wrapper = styled.div`
     margin-top: 2.5rem;
   }
   .error p,
-  p.error,
-  label.error {
+  p.error {
     color: ${props => props.theme.errorColor};
     font-weight: 600;
   }
-  @media (max-width: ${props => props.theme.sm}) {
+  @media (max-width: ${props => props.theme.xs}) {
     #dates {
-      grid-template-columns: auto auto;
-      grid-gap: 0.5rem;
-    }
-  }
-  @media (max-width: 336px) {
-    .react-datepicker-popper {
-      display: none;
+      grid-template-columns: auto;
+      grid-gap: 1.5rem;
     }
   }
 `
 
-const MyDatePicker = ({ label, date, setDate, id, disabled }) => (
-  <>
-    <label htmlFor={id}>{label}</label>
-    <DatePicker
-      dateFormat='dd/MM/yyyy à HH:mm'
-      disabled={disabled}
-      id={id}
-      locale='fr'
-      onChange={d => setDate(d)}
-      popperModifiers={{
-        preventOverflow: {
-          enabled: true,
-          escapeWithReference: false,
-          boundariesElement: 'viewport'
-        }
-      }}
-      selected={date}
-      showTimeSelect
-      timeCaption='Heure'
-      timeFormat='HH:mm'
-      timeIntervals={5}
-    />
-  </>
-)
+const days = [
+  { label: 'Lundi', value: 'mon' },
+  { label: 'Mardi', value: 'tue' },
+  { label: 'Mercredi', value: 'wed' },
+  { label: 'Jeudi', value: 'thu' },
+  { label: 'Vendredi', value: 'fri' },
+  { label: 'Samedi', value: 'sat' },
+  { label: 'Dimanche', value: 'sun' }
+]
+
+const initialOccurrence = {
+  mon: false,
+  tue: false,
+  wed: false,
+  thu: false,
+  fri: false,
+  sat: false,
+  sun: false
+}
 
 function NewEvent ({ id }) {
   const [name, setName] = useState('Mon Event')
@@ -103,13 +97,15 @@ function NewEvent ({ id }) {
   const [newGroup, setNewGroup] = useState('')
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
+  const [occurrence, setOccurrence] = useState(initialOccurrence)
+  const [showDays, setShowDays] = useState(false)
   const [description, setDescription] = useState('Une description')
   const [photos, setPhotos] = useState([])
   const [loading, setLoading] = useState(false)
   const [eventLoading, setEventLoading] = useState(false)
   const [nameError, setNameError] = useState('')
   const [groupError, setGroupError] = useState('')
-  const [dateError, setDateError] = useState('')
+  const [endDateError, setEndDateError] = useState('')
   const [descError, setDescError] = useState('')
 
   const { loading: groupLoading, groups } = useGroups()
@@ -119,33 +115,41 @@ function NewEvent ({ id }) {
   }, [groups])
 
   const validateStartDate = d => {
-    if (!d) return
     if (d > endDate) setEndDate(d)
     setStartDate(d)
   }
 
   const validateEndDate = d => {
-    if (!d) return
     if (d < startDate) setStartDate(d)
     setEndDate(d)
+  }
+
+  const handleShowDaysChange = ({ target: { checked } }) => {
+    if (!checked) setOccurrence(initialOccurrence)
+    setShowDays(checked)
+  }
+
+  const handleOccurrenceChange = value => ({ target: { checked } }) => {
+    setOccurrence({ ...occurrence, [value]: checked })
   }
 
   const save = () => {
     setNameError('')
     setGroupError('')
-    setDateError('')
+    setEndDateError('')
     setDescError('')
     if (!name) return setNameError('Veuillez saisir un nom')
-    if (!group && !newGroup) {
+    if (group === 'new' && !newGroup) {
       return setGroupError('Veuillez saisir le nom du groupe')
     }
     if (startDate.getTime() === endDate.getTime()) {
-      return setDateError(
+      return setEndDateError(
         'La date de fin doit être différente de la date de début'
       )
     }
     if (!description) return setDescError('Veuillez saisir une description :')
     setLoading(true)
+    setEventLoading(true)
     // TODO
   }
 
@@ -185,19 +189,21 @@ function NewEvent ({ id }) {
             onChange={e => setGroup(e.target.value)}
             value={group}
           >
+            <MenuItem value=''>
+              <em>Aucun</em>
+            </MenuItem>
+            <MenuItem value='new'>
+              <em>Nouveau groupe</em>
+            </MenuItem>
+            <Divider />
             {groups &&
               groups.map(g => (
                 <MenuItem key={g._id} value={g._id}>
                   {g.name}
                 </MenuItem>
               ))}
-            {!groupLoading && (
-              <MenuItem value=''>
-                <em>Nouveau groupe</em>
-              </MenuItem>
-            )}
           </Select>
-          {!groupLoading && group === '' && (
+          {!groupLoading && group === 'new' && (
             <TextField
               className='new-group'
               disabled={loading || eventLoading}
@@ -210,22 +216,50 @@ function NewEvent ({ id }) {
           )}
         </div>
         <div className='grid' id='dates'>
-          <MyDatePicker
+          <DatePicker
             date={startDate}
             disabled={loading || eventLoading}
-            id='start'
             label='Début :'
             setDate={validateStartDate}
           />
-          <MyDatePicker
+          <DatePicker
             date={endDate}
             disabled={loading || eventLoading}
-            id='end'
+            error={!!endDateError}
             label='Fin :'
             setDate={validateEndDate}
           />
         </div>
-        {dateError && <p className='error'>{dateError}</p>}
+        {endDateError && <p className='date-error error'>{endDateError}</p>}
+        <div id='occurrence'>
+          <FormControl component='fieldset'>
+            <FormGroup row>
+              <FormControlLabel
+                control={<Checkbox onChange={handleShowDaysChange} />}
+                label="Répétition de l'évènement"
+              />
+            </FormGroup>
+            {showDays && (
+              <FormGroup row>
+                {days.map(({ label, value }, index) => (
+                  <FormControlLabel
+                    className='daylabel'
+                    control={
+                      <Checkbox
+                        checked={occurrence[value]}
+                        color='primary'
+                        onChange={handleOccurrenceChange(value)}
+                        value={value}
+                      />
+                    }
+                    key={index}
+                    label={label}
+                  />
+                ))}
+              </FormGroup>
+            )}
+          </FormControl>
+        </div>
         <Description
           className={descError ? 'error' : ''}
           label={descError || 'Description :'}
@@ -255,14 +289,36 @@ function NewEvent ({ id }) {
   )
 }
 
-NewEvent.propTypes = { id: PropTypes.string }
+class FrLocalizedUtils extends DateFnsUtils {
+  getDateTimePickerHeaderText (date) {
+    return this.format(date, 'd MMM', { locale: this.locale })
+  }
+}
 
-MyDatePicker.propTypes = {
+const DatePicker = ({ label, date, setDate, disabled, error }) => (
+  <MuiPickersUtilsProvider locale={fr} utils={FrLocalizedUtils}>
+    <DateTimePicker
+      ampm={false}
+      cancelLabel='annuler'
+      disabled={disabled}
+      error={error}
+      format='d MMM yyyy à HH:mm'
+      hideTabs
+      label={label}
+      onChange={setDate}
+      value={date}
+    />
+  </MuiPickersUtilsProvider>
+)
+
+DatePicker.propTypes = {
   label: PropTypes.string.isRequired,
   date: PropTypes.object,
   setDate: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  error: PropTypes.bool
 }
+
+NewEvent.propTypes = { id: PropTypes.string }
 
 export default NewEvent
